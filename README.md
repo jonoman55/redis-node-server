eec62863c5049307c3acc2a315e69b4aa2d9fcba
jonoman55 <jonoman55@yahoo.com>
2022-04-23 21:01:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Node/Express and Redis Client
+
+Fetches and caches GitHub users public repo count.<br />
+Subsequent requests to /repos/:username will be faster on each fetch.
+
+## Installation
+
+Run the command below to install the necessary packages.
+
+### `npm install`
+
+## Available Scripts
+
+In the project, you can run:
+
+### `npm run dev`
+
+Runs the app in development mode with nodemon.<br />
+Express server listens on [http://localhost:5000](http://localhost:5000)
+Redis connects on [127.0.0.1:6379](127.0.0.1:6379])
+
+The server will reload if you make edits.<br />
+
+### `npm start`
+
+Runs the app without nodemon.<br />
+Express server listens on [http://localhost:5000](http://localhost:5000)
+Redis connects on [127.0.0.1:6379](127.0.0.1:6379])
---
+++ index.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import redis from 'redis';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './sample.env'
+});
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient({
+    port: REDIS_PORT
+});
+
+// Connect Redis client
+(async () => {
+    await client.connect();
+})();
+
+client.on('connect', () =>
+    console.log('::> Redis Client Connected')
+);
+client.on('error', (err) =>
+    console.log('<:: Redis Client Error', err)
+);
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Set Response
+function setResponse(username, repos) {
+    return `<h2>${username} has ${repos} public GitHub repos</h2>`;
+}
+
+// Make request to GitHub API for data
+async function getRepos(req, res, next) {
+    try {
+        console.log('Fetching data...');
+        const { username } = req.params;
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        const repos = data?.public_repos;
+        await client.setEx(username, 3600, repos);
+        // Set data to Redis
+        res.send(setResponse(username, repos));
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+}
+
+// Cache middleware
+async function cache(req, res, next) {
+    try {
+        const { username } = req.params;
+        const data = await client.get(username);
+        if (data) {
+            console.log('Data cached to redis...');
+            res.send(setResponse(username, data));
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get('/repos/:username', cache, getRepos);
+
+app.listen(PORT, () =>
+    console.log(`Server is now running on port: ${PORT}`)
+);
